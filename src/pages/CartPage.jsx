1d3036eb583f605e19193db964fb30e56de37cc3
doctordred111 <doctordred111@gmail.com>
2025-05-20 00:02:36 +0300

    update

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import styles from './CartPage.module.scss';
 import Cart from "../components/Cart/index.jsx";
 import CartForm from '../Forms/CartForm';
+import {useSelector} from 'react-redux';
+
 
 const CartPage = () => {
+    const favoritesMoto = useSelector(state => state.motorcycles.favoritesMoto);
+
     return (
         <div className={styles.cartPage}>
-            <CartForm />
-            <Cart position='normal' />
+
+            {favoritesMoto.length > 0 ? (
+                <>
+                <CartForm />
+                <Cart position='normal' />
+                </>
+            ) : (
+                <Cart position='normal' />
+            )}
+            
         </div>
     );
 };
